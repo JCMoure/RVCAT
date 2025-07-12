eb4eb0e00ac8e8c8c8275e2d11841ee0813899ce
JuanCarlos Moure <juancarlos.moure@uab.es>
2025-07-12 12:09:52 +0200

    INIT

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 import numpy as np
+
 from .program   import _program
 from .processor import _processor
 from .scheduler import _scheduler 
-from .isa       import _isa
+# from .isa       import _isa
 
 def load_program(program: str) -> None:
         _program.load_program(program)
@@ -10,12 +11,12 @@ def load_program(program: str) -> None:
 def load_processor(processor: str) -> None:
         _processor.load_processor(processor)
 
-def load_isa(isa: str) -> None:
-        _isa.load_isa(isa)
+# def load_isa(isa: str) -> None:
+#        _isa.load_isa(isa)
 
-def show_program_dependencies() -> None:
-    if _program.loaded:
-        print(_program.show_dependencies())
+# def show_program_dependencies() -> None:
+#    if _program.loaded:
+#        print(_program.show_dependencies())
 
 def show_program_annotated() -> None:
     if _program.loaded:
@@ -29,16 +30,16 @@ def show_program_performance() -> None:
     if _program.loaded:
         print(_program.show_static_performance_analysis())
 
-def show_program_memory() -> None:
-    if _program.loaded:
-        print(_program.show_memory_trace())
+# def show_program_memory() -> None:
+#    if _program.loaded:
+#        print(_program.show_memory_trace())
 
 def show_program() -> None:
     if _program.loaded:
         print(_program)
 
-def show_processor() -> None:
-        print(_processor)
+# def show_processor() -> None:
+#        print(_processor)
 
-def show_isa() -> None:
-        print(_isa)
+# def show_isa() -> None:
+#        print(_isa)
